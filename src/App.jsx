50989749b06657d6e33ff437fab2085793e8065b
nikki <nikkimehta2807@gmail.com>
2024-10-04 15:41:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+import Contact from './components/Contact/Contact'
+
+
+const App = () => {
+  return (
+    <div>
+    <Contact/>
+    </div>
+  )
+}
+
+export default App
